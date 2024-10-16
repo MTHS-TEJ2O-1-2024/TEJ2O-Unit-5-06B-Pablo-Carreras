f98ac64edf68b6aa4f95e4cb00b3f5af52dc11ed
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-16 10:41:14 -0400

    did the code

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo
+ * Created on: Sep 2024
+ * This program messures the disctance of an object 
 */
 
-basic.showString('Hello, World!')
+let disctanceToObject: number = 0
+
+//setup 
+basic.showIcon(IconNames.Happy)
+
+//find the disctance of the sonar 
+input.onButtonPressed(Button.A, function(){
+    basic.clearScreen()
+    disctanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(disctanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
